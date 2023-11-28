c573262acc4ca2ded0f7b8318c1037ea8a883c9e
Shunto Higa <e235721@ie.u-ryukyu.ac.jp>
2023-11-28 19:46:19 +0900

    report4_step1

--- app/src/main/java/jp/ac/uryukyu/ie/e235721/Hero.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e235721/Hero.java
@@ -8,11 +8,8 @@ package jp.ac.uryukyu.ie.e235721;
  *  boolean dead; //敵の生死状態。true=死亡。
  * Created by tnal on 2016/11/13.
  */
-public class Hero {
-    private String name;
-    private int hitPoint;
-    private int attack;
-    private boolean dead;
+
+public class Hero extends LivingThing {
 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
@@ -20,56 +17,9 @@ public class Hero {
      * @param maximumHP ヒーローのHP
      * @param attack ヒーローの攻撃力
      */
+
     public Hero (String name, int maximumHP, int attack) {
-        this.name = name;
-        hitPoint = maximumHP;
-        this.attack = attack;
-        dead = false;
-        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
-    }
-
-    public String getName(){
-        return this.name;
-    }
-
-    public int getHitPoint(){
-        return this.hitPoint;
-    }
-
-    public int getAttack(){
-        return this.attack;
-    }
-
-    public boolean getDead(){
-        return this.dead;
-    }
-
-    // Setterメソッド
-    public void setName(String name){
-        this.name = name;
-    }
-
-    public void setHitPoint(int hitPoint){
-        this.hitPoint = hitPoint;
-    }
-
-    public void setAttack(int attack){
-        this.attack = attack;
-    }
-
-    public void setDead(boolean dead){
-        this.dead = dead;
-    }
-
-    /**
-     * Enemyへ攻撃するメソッド。
-     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
-     * @param e 攻撃対象
-     */
-    public void attack(Enemy e){
-        int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, e.getName(), damage);
-        e.wounded(damage);
+        super(name, maximumHP, attack);
     }
 
     /**
@@ -77,11 +27,12 @@ public class Hero {
      * 指定されたダメージを hitPoint から引き、死亡判定を行う。
      * @param damage 受けたダメージ
      */
+
+    @Override
     public void wounded(int damage){
-        hitPoint -= damage;
-        if( hitPoint < 0 ) {
-            dead = true;
-            System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", name);
+        super.wounded(damage);
+        if(isDead()) {
+            System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", getName());
         }
     }
 }
---
+++ app/src/main/java/jp/ac/uryukyu/ie/e235721/LivingThing.java
@@ -0,0 +1,43 @@
+package jp.ac.uryukyu.ie.e235721;
+
+public class LivingThing {
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
+
+    public LivingThing(String name, int hitPoint, int attack) {
+        this.name = name;
+        this.hitPoint = hitPoint;
+        this.attack = attack;
+        this.dead = false;
+        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, hitPoint, attack);
+    }
+
+    public boolean isDead() {
+        return this.dead;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getHitPoint() {
+        return this.hitPoint;
+    }
+
+    public void attack(LivingThing opponent) {
+        if (!dead) {
+            int damage = (int) (Math.random() * attack);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, opponent.getName(), damage);
+            opponent.wounded(damage);
+        }
+    }
+
+    public void wounded(int damage) {
+        hitPoint -= damage;
+        if (hitPoint < 0) {
+            dead = true;
+        }
+    }
+}
